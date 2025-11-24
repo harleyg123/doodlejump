b577e127847a06e7f668acacb4d4c9c6066ea6d2
harleyg123 <harley@godsal.net>
2025-11-24 12:02:22 -0300

    Test 1

---
+++ main.py
@@ -0,0 +1,61 @@
+import pygame
+import sys
+from pygame.locals import QUIT, KEYDOWN, K_SPACE
+
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((600, 600))
+clock = pygame.time.Clock()
+dt = clock.get_time()
+
+sprite = pygame.image.load("Doodle Jump\\blue-lik-right.png")
+sprite_rect = sprite.get_rect()
+sprite_rect.midbottom = (300, 450)  # starting position on ground
+
+
+GROUND_Y = 450
+GRAVITY = 0.6
+jump_velocity = -12
+velocity_y = 0
+is_jumping = False
+move_speed = 500
+#C:\Users\G2520640\Documents\doodle\Doodle Jump\doodlestein-right@2x.png
+#C:\Users\G2520640\Documents\doodle\Doodle Jump\game-tiles-space.png
+
+while True:
+    dt = clock.tick(60)/1000
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_SPACE and not is_jumping:
+                velocity_y = jump_velocity
+                is_jumping = True
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+                sprite_rect.x -= dt * move_speed
+
+        if keys[pygame.K_d]:
+                sprite_rect.x += dt * move_speed
+    print("1")
+    if is_jumping:
+        velocity_y += GRAVITY
+        sprite_rect.y += velocity_y
+
+        # Landing on ground
+        if sprite_rect.bottom >= GROUND_Y:
+            sprite_rect.bottom = GROUND_Y
+            velocity_y = 0
+            is_jumping = False
+
+    screen.fill("white")
+    pygame.draw.line(screen, "black", (0, GROUND_Y), (600, GROUND_Y), 4)
+
+    screen.blit(sprite, sprite_rect)
+
+    pygame.display.update()
